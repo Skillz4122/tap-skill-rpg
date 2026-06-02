7642631a99ac62296cc5f316232e9c179ef2b06c
SimpleCode-88 <jburton0526@gmail.com>
2026-06-02 17:22:12 -0500

    adding wood cutting functionality

--- src/screens/forest.js
+++ src/screens/forest.js
@@ -13,7 +13,9 @@ export function renderForestScreen(app) {
           🌳
         </div>
 
-        <p>HP: 20 / 20</p>
+      <p id="tree-hp">HP: 20/20</p>
+      <p id="logs">Logs: 0</p>
+      <p id="xp">XP: 0</p>
 
         <button id="tree-button">
           Chop Tree
@@ -23,15 +25,58 @@ export function renderForestScreen(app) {
     </main>
   `;
 
+  let treeHP = 20;
+  let logs = 0;
+  let xp = 0;
+
+  function updateForestUI() {
+  document.querySelector("#tree-hp").textContent =
+    `HP: ${treeHP}/20`;
+  if (treeHP <= 0) {
+
+  logs += 2;
+  xp += 10;
+
+  document.querySelector("#tree-button")
+    .disabled = true;
+
+  document.querySelector("#tree-hp")
+    .textContent = "Tree Cut Down";
+
+  setTimeout(() => {
+
+    treeHP = 20;
+
+    document.querySelector("#tree-button")
+      .disabled = false;
+
+    updateForestUI();
+
+  }, 1500);
+}
+
+  document.querySelector("#logs").textContent =
+    `Logs: ${logs}`;
+
+  document.querySelector("#xp").textContent =
+    `XP: ${xp}`;
+}
+
+
   document
     .querySelector("#back-button")
     .addEventListener("click", () => {
       console.log("Back To Village");
     });
 
-  document
-    .querySelector("#tree-button")
-    .addEventListener("click", () => {
-      console.log("Chop Tree");
-    });
+ document
+  .querySelector("#tree-button")
+  .addEventListener("click", () => {
+
+    treeHP--;
+
+    updateForestUI();
+
+  });
+
 }
